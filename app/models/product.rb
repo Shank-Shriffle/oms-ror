e1dcc4183e3ae2c4485b085379c020eedc1e7a17
ShankShriffle <shashankp@shriffle.com>
2022-08-23 20:25:30 +0530

    Product Controller,View,Validate, Fixed: Navbar

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,4 @@
 class Product < ApplicationRecord
+  validates :name, presence: true, uniqueness: {message: "Product already exists!"}
+  validates :price, :quantity, numericality: { only_integer: true }
 end
