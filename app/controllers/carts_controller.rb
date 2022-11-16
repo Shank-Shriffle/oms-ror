b0f7f2c34648d615d5959f65348367692b9e8343
Shank-001 <patilshashank929@gmail.com>
2022-11-16 12:46:18 +0530

    Unaware of Changes

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,8 +1,22 @@
 class CartsController < ApplicationController
+  def create
+    @cart = Cart.new(cart_params)
+
+    if @cart.save
+
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   def show
     @order_products = current_user.order_products
     # Check if query is correct or try to use current_user.order_products.
   end
 
-# Only show action needed.
+  private
+  def cart_params
+    params.require(:cart).permit(:customer_id)
+  end
+# Only create & show action needed.
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,6 @@
 class Customer < User
   has_many :orders, dependent: :destroy
   has_many :order_products, through: :orders
+  has_one :cart, dependent: :destroy
+  has_many :order_products, through: :cart
 end
